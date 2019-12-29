f195c06adc179b273050a512a75f48e89d6c5cee
Daniel Lauzon <daniel.lauzon@gmail.com>
2019-12-29 00:09:11 -0500

    Refactored out auth and token things

--- README.md
+++ README.md
@@ -1,33 +1,16 @@
 # gphotos-googleapis
 
-- Get a token and list all photos
+- Use a refresh token and list all photos
 
 ## TODO
 
-- Document provisioning App Credentials
-- Remove a level from `oauth2.keys.json`
 - Implement refreshTokensDB.json
   - persist refresh token by account
-- yargs: verify,renew,noauth,user
-- extract Auth/TokenManagement into npm
+- yargs: verify
 - renovate
-- `google-auth-library` is included in `googleapis`, switch to that
-
-## OAuth2 Flow
-
-This is the OAuth2 flow as descibed in <https://github.com/googleapis/google-auth-library-nodejs#oauth2> and <https://github.com/googlesamples/google-photos/tree/master/REST/PhotoFrame>.
-
-This is meant to get local credententials, for local development (i.e. the OAuth callback is on <http://127.0.0.1>).
-
-- Prepare som credentials in the [Google Developpers Console]
-  - Select a (possibly new) project
-  - Enable the API's you want to use in the APIs & Services Section/Library
-  - Create OAuth client ID from the dropdown and give it a  name: `OAuth CLient` for example
-    - Application type: Web Application, and give it a name
-    - Set the authorized JavaScript origin to <http://127.0.0.1>
-    - Set authorized redirect URL to <http://127.0.0.1:8080/auth/google/callback>
 
 ## References
 
+- <https://github.com/daneroo/get-me-a-googleapi-refresh-token>
 - <https://github.com/googleapis/google-auth-library-nodejs#oauth2>
 - <https://github.com/googlesamples/google-photos/tree/master/REST/PhotoFrame>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-const { usingRefreshToken, validateScope, makeRefreshTokenWithWebFlow } = require('./auth')
 const fsPromises = require('fs').promises
+const { usingRefreshToken } = require('@daneroo/get-me-a-googleapi-refresh-token/src/auth')
 
 // Download your OAuth2 configuration from the Google Console/APIs ad Services/Credentials
-const refreshTokenDBFile = './refreshTokenDB.json'
-const keys = require('../oauth2.keys.json')
-const scope = [
-  'https://www.googleapis.com/auth/userinfo.profile', // so I can get name
-  'https://www.googleapis.com/auth/photoslibrary.readonly'
-]
+const keysFile = './oauth2.keys.json'
+const tokenFile = './tokens.json'
 
 main().catch(console.error)
 
@@ -15,56 +11,20 @@ main().catch(console.error)
  * Start by acquiring a pre-authenticated oAuth2 client.
  */
 async function main () {
-  const renew = true
-  const verify = true
-  // First Part get an authenticaed OAuth2 token - and serialize it to file
-  if (renew) {
-    // const { tokens, tokenInfo } = await makeRefreshTokenWithWebFlow(keys, scope)
-    const refreshTokenDBEntry = await makeRefreshTokenWithWebFlow(keys, scope)
-    console.info(`Refresh token acquired. (${refreshTokenDBEntry.id})`)
-    // console.debug('-', { refreshTokenDBEntry })
-
-    await fsPromises.writeFile(refreshTokenDBFile, JSON.stringify(refreshTokenDBEntry))
-    console.info('Refresh Token persisted.')
-  }
-
-  // Second part de-seraialze the token and us it to make an authenticaed API call
-  if (verify) {
-    const { refreshToken } = JSON.parse(await fsPromises.readFile(refreshTokenDBFile))
-    console.info('Refresh token read.')
-
-    // TODO: add optional validation to usingRefreshToken)
-    const oAuth2Client = await usingRefreshToken(keys, refreshToken)
-    console.info('Client acquired.')
-
-    const scopesOk = await validateScope(oAuth2Client, scope)
-    if (!scopesOk) {
-      throw new Error('Missing authorization scopes')
-    }
-
-    {
-      console.log('Calling OAuth userInfo - to validate token')
-      const url = 'https://www.googleapis.com/oauth2/v3/userinfo'
-      const { data: { name, sub: id } } = await oAuth2Client.request({ url })
-      console.log(`Worked cause I know your name:${name} (${id})`)
-    }
-    {
-      console.log('Calling People API - to validate token')
-      const url = 'https://people.googleapis.com/v1/people/me?personFields=names'
-      const { data: { resourceName, names } } = await oAuth2Client.request({ url })
-      console.log(`Worked cause I know your name:${names[0].displayName} (${resourceName})`)
-    }
-  }
-  await listItems()
-}
-
-async function listItems () {
-  const { refreshToken } = JSON.parse(await fsPromises.readFile(refreshTokenDBFile))
+  const { clientId, clientSecret, scopes } = JSON.parse(await fsPromises.readFile(keysFile))
+  console.info('Keys read.')
+  // read the refreshTOken
+  const { refreshToken } = JSON.parse(await fsPromises.readFile(tokenFile))
   console.info('Refresh token read.')
 
-  const oAuth2Client = await usingRefreshToken(keys, refreshToken)
+  // TODO: add optional validation to usingRefreshToken)
+  const oAuth2Client = await usingRefreshToken({ clientId, clientSecret, refreshToken, scopes, verbose: true })
   console.info('Client acquired.')
 
+  await listItems(oAuth2Client)
+}
+
+async function listItems (oAuth2Client) {
   const params = {
     pageSize: 100 // max is 100
     // pageToken : res.nextPageToken
